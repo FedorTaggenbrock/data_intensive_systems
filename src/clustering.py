9839d3a15c4300547757f1e7f6337d37fb31005a
AbeWolthuis <abe.wolthuis@gmail.com>
2023-05-25 14:25:03 +0200

    -

--- src/clustering.py
+++ src/clustering.py
@@ -118,7 +118,7 @@ def evaluate_kModes(data: RDD, centroids: list, distance = scipy.spatial.distanc
 
 def clustering_test1():
     # Testing code
-    print('update 14:16')
+    print('update 14:24')
     spark = SparkSession.builder.appName("Clustering").getOrCreate()
 
     data = spark.sparkContext.parallelize([
