21d9a66c9846943fb70c13dc6db9aea53eb1b79f
AbeWolthuis <abe.wolthuis@gmail.com>
2023-05-27 17:10:13 +0200

    Evaluation of kmodes almost done. Not sure if it works with the categorical predicted centroids though...

--- src/evaluate_clustering.py
+++ src/evaluate_clustering.py
@@ -31,43 +31,69 @@ def evaluate_clustering(data: RDD, predicted_centroids: list, clustering_setting
     return evaluation_metrics
     
 
-def evaluate_kModes(data: RDD, clustering_results: list, distance: Callable, perfect_centroids = Union[None, tuple]) -> dict:
+from typing import List, Tuple, Callable, Union
+from pyspark import RDD
+
+def evaluate_kModes(data: RDD, clustering_settings: dict, clustering_result, distance_function: Callable, perfect_centroids: Union[None, Tuple]) -> dict:
     """
     Evaluate the clustering of the given data using the given centroids and k-modes algorithm.
 
     Args:
-        clustering_results: For each clustering setting, contains predicted centroids, e.g. for kmodes: [(predicted_centroids, (k, init_mode)), ...].
+        clustering_results: For each clustering setting, contains predicted centroids, e.g. for kmodes: [([1.0, 2.0, 2.5], ('k', 'init_mode')), ...].
+                            List[  Tuple[ List[float], Tuple[str] ]  ]
     """
 
     results = []
 
-    for result in clustering_results:
+    for result in clustering_result:
         # Extract the centroids for this setting
-        centroids: tuple = result[0]
+        centroids = result[0]
+        num_clusters = len(centroids)
 
-        # For each point, find its closest centroid and map it as (centroid, (distance, 1))
-        closest_centroids = data.map(lambda point: (min(centroids, key=lambda centroid: distance(point, centroid)), (distance(point, min(centroids, key=lambda centroid: distance(point, centroid))), 1)))
+        # For each point, find its closest centroid and map it as (point, centroid)
+        closest_centroids = data.map(lambda point: (point, min(centroids, key=lambda centroid: distance_function(point, centroid))))
 
-        # Compute the average and variance of the distances to each centroid
-        sum_distances_and_counts = closest_centroids.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+        # Group points by centroid and compute the total distance and count.
+        # Example structure: {centroid1: {'total_distance': 30,
+        #                                 'count': 10, 
+        #                                 'distances': [1, 2, 3, 2, 3, 2, 1, 2, 3, 1]},
+        #                     centroid2: {...}, ...}
+        cluster_metrics = closest_centroids.groupByKey().mapValues(lambda values: {
+            'total_distance': sum([v[1] for v in values]), # Maybe use np.sum() instead?
+            'count': len(values),
+            'distances': [v[1] for v in values]   # we also need the list of distances to calculate the variance
+        }).collectAsMap()
 
-        average_within_cluster_distance = sum_distances_and_counts.mapValues(lambda x: x[0] / x[1]).collectAsMap()
-        within_cluster_variance = sum_distances_and_counts.mapValues(lambda x: (x[0] / x[1]) ** 2).collectAsMap()
+        # Compute the average distance within each cluster, for each centroid
+        for key in cluster_metrics:
+            cluster_metrics[key]['average_distance'] = cluster_metrics[key]['total_distance'] /   cluster_metrics[key]['count'] # type: ignore
+        # Compute the overall average distance across all clusters (aka sum the within-cluster-distances and divide by the number of clusters)
+        average_within_cluster_distance = np.sum([v['average_distance'] for v in cluster_metrics.values()]) / num_clusters
+
+        # Compute the variance within each cluster
+        for key in cluster_metrics:
+            cluster_metrics[key]['variance'] = np.var(cluster_metrics[key]['distances'])
+        # Compute the overall average variance across all clusters
+        average_within_cluster_variance = np.sum([v['variance'] for v in cluster_metrics.values()]) / num_clusters
 
         # Calculate distances between the centroids
-        between_cluster_distance = np.mean(distance.pdist(centroids, metric='jaccard'))
+        '''Possible options are:
+        - Silhouette Score: The silhouette score is a measure of how similar an object is to its own cluster (cohesion) compared to other clusters (separation). The silhouette score ranges from -1 to 1, where a high value indicates that the object is well matched to its own cluster and poorly matched to neighboring clusters.
+        - Davies-Bouldin Index: This index is a metric of cluster separation. It calculates the average similarity measure of each cluster with its most similar cluster, where similarity is the ratio of within-cluster distances to between-cluster distances. Lower values indicate better clustering.
+        - Calinski-Harabasz Index: Also known as the Variance Ratio Criterion, this index is a ratio of the between-cluster dispersion mean and the within-cluster dispersion. Higher values indicate better clustering.
+        '''
 
         # If perfect_centroids are given, calculate average deviation from perfection
         if perfect_centroids:
-            average_centroid_deviation = sum([distance(a, b) for a, b in zip(centroids, perfect_centroids)]) / len(centroids)
+            average_centroid_deviation = sum([distance_function(a, b) for a, b in zip(centroids, perfect_centroids)]) / len(centroids)
         else:
             average_centroid_deviation = None
 
         results.append({
             'settings': result[1],
             'average_within_cluster_distance': average_within_cluster_distance,
-            'between_cluster_distance': between_cluster_distance,
-            'within_cluster_variance': within_cluster_variance,
+            'average_within_cluster_variance': average_within_cluster_variance,
+            # Add between-cluster metrics here
             'average_centroid_deviation': average_centroid_deviation,
         })
 
