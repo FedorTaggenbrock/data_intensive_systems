1da284ad52e04fecc418920c5b6dafb0a43f96d5
FedorTaggenbrock <fedor752@gmail.com>
2023-05-31 17:30:14 +0200

    Testing distance function for new routes

--- src/distance_function.py
+++ src/distance_function.py
@@ -2,6 +2,7 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import FloatType
 
 def route_distance(route1, route2):
+    print(route1)
     columns = route1.__fields__
     intersection = 0
     union = 0
