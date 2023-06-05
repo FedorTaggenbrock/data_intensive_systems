05f1b5fb1b3aaf7d50bc62ec8db0459f6686a712
FedorTaggenbrock <fedor752@gmail.com>
2023-06-05 12:20:33 +0200

    Storing the product in a dict

--- src/distance_function.py
+++ src/distance_function.py
@@ -3,14 +3,18 @@ from pyspark.sql.types import FloatType
 import numpy as np
 
 def route_distance(route1, route2):
-    columns = route1.__fields__[1:]
-    print(columns)
-    intersection = 0
-    union = 0
-    for column in columns:
-        if any(route1[column]) or any(route2[column]):
-            union += 1
-            if any(route1[column]) and any(route2[column]):
-                intersection += 1
-    return float(intersection) / union if union != 0 else 0.0
+
+    intersection = np.logical_and(route1, route2).sum(axis=1)
+    union = np.logical_or(route1, route2).sum(axis=1)
+    return 1 - intersection / union
+
+    #columns = route1.__fields__[1:]
+    # intersection = 0
+    # union = 0
+    # for column in columns:
+    #     if any(route1[column]) or any(route2[column]):
+    #         union += 1
+    #         if any(route1[column]) and any(route2[column]):
+    #             intersection += 1
+    # return float(intersection) / union if union != 0 else 0.0
 
