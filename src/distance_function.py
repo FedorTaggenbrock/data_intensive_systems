009677df118c005f6fb3ed711c0bd6d5b61b28d8
FedorTaggenbrock <fedor752@gmail.com>
2023-06-05 17:14:09 +0200

    working on distance function and centroids in kmodes

--- src/distance_function.py
+++ src/distance_function.py
@@ -7,13 +7,18 @@ def route_distance(route1, route2):
     intersection = 0
     union = 0
     for column in columns:
-        if any(route1[column]) or any(route2[column]):
+        trip1 = any(route1[column])
+        trip2 = any(route2[column])
+        if trip1 or trip2:
             union += 1
-            if any(route1[column]) and any(route2[column]):
+            if trip1 and trip2:
                 intersection += dictionary_distance(route1[column], route2[column])
     return float(intersection) / union if union != 0 else 0.0
 
 def dictionary_distance(dict1, dict2):
     #This function computes the euclidean distance for dict representations of (sparse) vectors.
     #The get method is used to return a default value of 0 for keys that are not present in one of the dictionaries
+    for product in set(dict1) | set(dict2):
+        print("prod in dict1", dict1.get(product, 0))
+
     return math.sqrt(np.sum([(int(float(dict1.get(product, 0))) - int(float(dict2.get(product, 0)))) ** 2 for product in set(dict1) | set(dict2)]))
