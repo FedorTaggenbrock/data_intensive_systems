8ee2c234d59db5bb7a2d3293189a0710b3e40866
FedorTaggenbrock <fedor752@gmail.com>
2023-06-05 11:34:40 +0200

    Storing the product in a dict

--- src/parse_data.py
+++ src/parse_data.py
@@ -1,8 +1,11 @@
 import json
 import pandas as pd
 import inspect
-from pyspark.sql.functions import collect_list
+from pyspark.sql.functions import collect_list, udf
 from pyspark.sql import SparkSession
+from pyspark.sql.types import MapType, IntegerType, StringType
+
+
 def parse_json_data(json_path='data_intensive_systems/data/ex_example_route.json', debug_flag = False):
     """Parse the data from the json file to a pandas df."""
 
@@ -78,52 +81,18 @@ def encode_data(spark: SparkSession, df: pd.DataFrame, debug_flag =False):
 
     #Combine all the rows with the same route_id in such a way that all the different values per column are combined into a list.
     from_to_cols = [col for col in spark_df.columns if col not in ['route_id', 'product']]
-    spark_df = spark_df.groupBy("route_id").agg(*[collect_list(c).alias(c) for c in from_to_cols])
+    list_to_dict_udf = udf(list_to_dict, MapType(IntegerType(), StringType()))
+    spark_df = spark_df.groupBy("route_id").agg(*[list_to_dict_udf(collect_list(c)).alias(c) for c in from_to_cols])
 
     if debug_flag:
         print("Spark dataframe after encoding:")
         spark_df.show(truncate=False)
-        print("Intermediate pandas dataframe")
-        print(result)
+        # print("Intermediate pandas dataframe")
+        # print(result)
         print("product list:")
         print(product_list)
 
     return spark_df, product_list
 
-
-    # if encode_style == 'all':
-    #     if one_hot_encode:
-    #         # One-hot encode the cities
-    #         df = pd.get_dummies(df, columns=['from', 'to'])
-    #
-    #     # Scale the one-hot features appropriately;
-    #     # To do this, first select all one-hot columns.
-    #     # one_hot_cols = [col for col in df.columns if col.startswith('from_') or col.startswith('to_')]
-    #
-    #
-    # elif encode_style == 'num_only':
-    #     # Drop categorical columns
-    #     df.drop(columns=['from', 'to'], inplace=True)
-    # elif encode_style == 'cat_only':
-    #     # Drop numerical columns (i.e. keep only the categorical columns)
-    #     df.drop(columns=[col for col in df.columns if col not in ['from', 'to']], inplace=True)
-    #     if one_hot_encode:
-    #         # One-hot encode the cities
-    #         df = pd.get_dummies(df, columns=['from', 'to'])
-    # else:
-    #     raise NotImplementedError(f'encode_style "{encode_style}" not implemented in function "{inspect.currentframe().f_code.co_name}."')
-    #
-    # def parse_test():
-    #     # Write comment that says this is only used for testing
-    #     df = parse_json_data()
-    #     df = encode_data(df, encode_style='all_')
-    #
-    #     # Print formatting
-    #     pd.set_option('expand_frame_repr', True)
-    #     pd.set_option('display.width', 100)
-    #     pd.set_option('display.max_columns', 100)
-    #
-    #     print(df.head())
-    #
-    # if __name__ == '__main__':
-    #     parse_test()
+def list_to_dict(product_list, list):
+    return {product_list[i]: x for i, x in enumerate(list)}
