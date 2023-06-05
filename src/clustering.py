b0ed2a45403af1c6ce0ac238cd8dd3721896500f
FedorTaggenbrock <fedor752@gmail.com>
2023-06-05 18:03:41 +0200

    insanity debugging again

--- src/clustering.py
+++ src/clustering.py
@@ -47,6 +47,7 @@ def kModes(spark_instance: SparkSession, data: RDD, k: int, clustering_settings)
     two_routes = data.take(2)
     print("Testing route_distance inside Kmodes")
     print(route_distance(two_routes[0], two_routes[1]))
+    print("data:", data.collect())
 
     #clusters = data.map(lambda point: (min(centroids, key=lambda centroid: route_distance(point, centroid)), point)).groupByKey()
     #print("clusters1 = ", clusters.collect())
