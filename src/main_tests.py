e9edefd8c77b2e5b041a620e26666ca35f52d726
FedorTaggenbrock <fedor752@gmail.com>
2023-06-05 17:00:40 +0200

    insanity debugging

--- src/main_tests.py
+++ src/main_tests.py
@@ -17,6 +17,7 @@ from parse_data import parse_json_data, encode_data
 
 from data_visualization import plot_routes, plot_results
 
+from clustering import run_clustering
 from distance_function import route_distance
 
 
@@ -85,68 +86,3 @@ def plot_test():
 
 
 
-
-def run_clustering(spark_instance: SparkSession, clustering_settings: dict, data: RDD) -> list[tuple]:
-    '''Define variables to store results.'''
-    # E.g. for kmodes: [(predicted_centroids, (k, init_mode)), ...]
-    results = []
-
-    # Check which clustering algortihm to run
-    if clustering_settings['clustering_algorithm'] == 'kmodes':
-        for current_k in clustering_settings['k_values']:
-            # TODO in the future add other parameters here.
-
-            # Run clustering with current parameters
-            predicted_centroids = kModes(
-                spark_instance=spark_instance,
-                data=data,
-                k=current_k,
-                clustering_settings=clustering_settings
-            )
-
-            # Store the settings, model, and metrics
-            results.append((predicted_centroids, {'k': current_k}))
-    else:
-        print("Clustering algorithm setting not recognized in run_and_tune().")
-
-    return results
-
-
-def kModes(spark_instance: SparkSession, data: RDD, k: int, clustering_settings) -> list:
-    """
-    Perform k-modes clustering on the given data. Assumes only one-hot encoded data?
-
-    Args:
-        distance (function): The distance function to use for clustering.
-        data (RDD): The RDD containing the data to cluster.
-        k (int): The number of clusters to create.
-        max iterations (int): The maximum number of iterations to perform.
-
-    Returns:
-        list: A list of the centroids of the clusters.
-    """
-
-    centroids = [x for x in data.takeSample(withReplacement=False, num=k)]
-
-    # Iterate until convergence or until the maximum number of iterations is reached
-    for i in range(clustering_settings["max_iterations"]):
-        if clustering_settings["debug_flag"]:
-            print("centroids = ", centroids)
-
-        # Assign each point to the closest centroid
-        clusters = data.map(
-            lambda point: (min(centroids, key=lambda centroid: clustering_settings["distance_func"](point, centroid)), point)).groupByKey()
-
-        print("clusters1 = ", clusters.collect())
-
-        # Compute new centroids as the mode of the points in each cluster.
-        newCentroids = clusters.mapValues(lambda arrays: tuple([mode(x) for x in zip(*arrays)])).collect()
-
-        # print("newCentroids = ", newCentroids)
-
-        # Update centroids
-        for oldCentroid, newCentroid in newCentroids:
-            index = centroids.index(oldCentroid)
-            centroids[index] = newCentroid
-
-    return [list(x) for x in centroids]
