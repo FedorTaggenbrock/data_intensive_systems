1486426beef8b60c541536c31969f74933866d65
DouwedeKok <Douwedekok@gmail.com>
2023-06-06 15:44:37 +0200

    plot encode
    plot encode

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -2,6 +2,20 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.decomposition import PCA
+from pyspark.sql import SparkSession
+from parse_data import parse_json_data, encode_data
+from distance_function import route_distance
+from clustering import run_clustering
+import matplotlib.pyplot as plt
+
+def plot_encode():
+    # Opletten dat bij het parsen de hoeveelheden van stad A-> stad B wel goed samengevoegd worden. Zie nu twee keer dezelfde from->to staan bij route 1 namelijk.
+    pd_df, num_routes = parse_json_data('../data/ex_example_route.json')
+    spark = SparkSession.builder.appName("Clustering").getOrCreate()
+    print("Initialized Spark.")
+    encoded_spark_df, product_list = encode_data(spark, pd_df, False)
+    encoded_pd_df = encoded_spark_df.toPandas()
+    print(encoded_pd_df.to_string())
 
 def plot_routes(data:pd.DataFrame):
     le = LabelEncoder()
@@ -17,13 +31,23 @@ def plot_routes(data:pd.DataFrame):
     principal_components = pca.fit_transform(scaled_df)
 
     # Convert the principal components into a DataFrame
-    principal_df = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
+    pr_df = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
 
-    print(principal_df)
+    plt.figure(figsize=(10, 8))
+    plt.scatter(pr_df['PC1'], pr_df['PC2'])
+    plt.title('Scatter plot of PC1 vs PC2')
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.show()
 
+
+    print('')
     # Transform the data
     # df = pca.fit_transform(data)
     # print(df.shape)
 
 def plot_results(results):
     pass
+
+# if __name__ == '__main__':
+#     plot_test()
