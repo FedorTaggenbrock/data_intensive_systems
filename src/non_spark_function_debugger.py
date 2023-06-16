8863601c59c3becb5f2050c66926fef8dd204e5a
AbeWolthuis <abe.wolthuis@gmail.com>
2023-06-16 12:45:26 +0200

    -

--- src/non_spark_function_debugger.py
+++ src/non_spark_function_debugger.py
@@ -1,7 +1,12 @@
-# from parse_data import parse_json_data
-from parse_data_3 import parse_json_data_3 # yo die oude gaf errors met importeren, maar idk of dit het doet
+from parse_data import parse_json_data
+# from parse_data_3 import parse_json_data_3
 
-#pd_df, num_routes = parse_json_data(json_path="../data/10000_0.25_actual_routes.json")
-pd_df, num_routes = parse_json_data_3(json_path="../data/10_standard_route.json")
+def main():
+    #pd_df, num_routes = parse_json_data(json_path="../data/10000_0.25_actual_routes.json")
+    pd_df, num_routes = parse_json_data(json_path="../data/10_standard_route.json") 
+    '''yo dit path doet het niet, je moet checken of je op colab zit, en als dat zo is ../data aan je path appenden (er vanuitgaande dat die file daar dan is)'''
 
-print(pd_df)
+    print(pd_df)
+
+if __name__ == "__main__":
+    main()
